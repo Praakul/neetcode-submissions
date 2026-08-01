fc6015a0b14d8b595e2929fdccc42912f6d7865d
Prajwal Kulkarni <prajwalkulkarni098@gmail.com>
2026-08-02 00:18:33 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(char for char in s if char.isalnum()).lower()
+        leng = len(s)
+        for i in range(leng):
+            print(f's[i]:{s[i]}, s[leng - 1 -i]:{s[leng -1 - i]}')
+            if not s[i] == s[leng -1 - i]:
+                return False 
+        return True
+        
